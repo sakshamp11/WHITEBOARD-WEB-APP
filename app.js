1665943464d77d3fc4a8072a40dc63560bde7108
sakshamp11 <rimpi.saksham@gmail.com>
2022-06-30 19:06:00 +0530

    first commit

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ let app = express();
 app.use(express.static('public'))
 // http://localhost:5000
 
-// let port = process.env.PORT || 5000;
-// let server = app.listen(port, () => {
-//  console.log('app.Listern is Working');
-// })
-let server=app.listen(5000,()=>{
-    console.log("kl");
+let port = process.env.PORT || 5000;
+let server = app.listen(port, () => {
+ console.log('app.Listern is Working');
 })
+// let server=app.listen(5000,()=>{
+//     console.log("kl");
+// })
 
 
 let io = socket(server);
